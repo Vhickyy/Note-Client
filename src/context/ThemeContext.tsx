ce4348a0620cebdf8d20f6fcc5669f87cfef48f1
Victoria Okonnah <victoriaokonnah@gmail.com>
2023-11-20 03:21:56 -0800

    theme cotext done

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,10 +1,32 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
-const ThemeContext = createContext({});
+type ContextProp = {
+    theme: string
+    toggleTheme: () => void
+}
+
+const ThemeContext = createContext({} as ContextProp);
+
+const getTheme = () : "dark" | "light" => {
+    const localTheme = localStorage.getItem("note-theme");
+    document.body.classList.toggle("dark",localTheme == "dark");
+    return localTheme ? localTheme as "dark" | "light" : "light";
+}
 
 export const ThemeProvider = ({children}:{children:React.ReactNode}) => {
+    const [theme, setTheme] = useState<"light" | "dark">(getTheme());
+    
+    const toggleTheme = () => {
+        if(theme === "light"){
+            setTheme("dark");
+        }else setTheme("light");
+        document.body.classList.toggle("dark",theme == "dark");
+        localStorage.setItem("note-theme",theme)
+    }
+    
+
     return (
-        <ThemeContext.Provider value={{}}>
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
             {children}
         </ThemeContext.Provider>
     )
