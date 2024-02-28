3e6cd8df26789d3b412b63a466ebe1e913abded3
Victoria Okonnah <victoriaokonnah@gmail.com>
2024-02-28 16:12:55 +0100

    removed cors to test

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,8 +3,8 @@ import { NoteType, ProjectType, User } from "../types/types";
 
 
 export const customFetch = axios.create({
-        // baseURL: "http://localhost:8000/api",
-        baseURL: "https://note-backend-boze.onrender.com/api",
+        baseURL: "http://localhost:8000/api",
+        // baseURL: "https://note-backend-boze.onrender.com/api",
         headers: {
             "Content-Type": "application/json",
             // "Access-Control-Allow-Headers":
